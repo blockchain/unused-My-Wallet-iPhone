b49a7e5926f68e62b27adaed8f615660573c6fc4
Ben Reeves <zootreeves@gmail.com>
2014-07-28 22:22:33 +0100

    Chekc for guid and sharedKey rather than app.wallet

--- Blockchain/AccountViewController.m
+++ Blockchain/AccountViewController.m
@@ -54,7 +54,7 @@
 // called after user logs in
 - (void)loadWebView
 {
-    if (app.wallet)
+    if ([app guid] && [app sharedKey])
     {
         NSString *requestString = [NSString stringWithFormat:@"%@wallet/iphone-view?guid=%@&sharedKey=%@&device=iphone", WebROOT, [app guid], [app sharedKey]];
         [webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:requestString]]];

394aa9dafcba4a9c04532597d2f71889b1f5d949
Mark Pfluger <mark@pfluegerworld.de>
2014-10-16 17:53:21 -0700

    Animations for first start

--- Blockchain/BCWelcomeView.m
+++ Blockchain/BCWelcomeView.m
@@ -14,6 +14,9 @@
 
 @implementation BCWelcomeView
 
+UIImageView *imageView;
+float moveImageDown;
+
 -(id)init
 {
     AppDelegate *appDelegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
@@ -24,30 +27,71 @@
     if (self) {
         self.backgroundColor = [UIColor whiteColor];
         
+        // Logo
         UIImage *logo = [UIImage imageNamed:@"welcome_logo"];
-        UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(10, 30, logo.size.width, logo.size.height)];
+        imageView = [[UIImageView alloc] initWithFrame:CGRectMake(10, 30, logo.size.width, logo.size.height)];
         imageView.image = logo;
+        imageView.alpha = 0;
         
         CGFloat halfScreenHeight = window.frame.size.height / 2 - (BUTTON_HEIGHT) - 15;
         CGFloat halfScreenWidth = window.frame.size.width / 2;
         imageView.center = CGPointMake(halfScreenWidth, halfScreenHeight);
+        CGRect frame = imageView.frame;
+        moveImageDown = 82;
+        frame.origin.y += 82;
+        imageView.frame = frame;
         
         [self addSubview:imageView];
         
+        // Buttons
         self.createWalletButton = [UIButton buttonWithType:UIButtonTypeCustom];
         self.createWalletButton.frame = CGRectMake(0, self.frame.size.height - (BUTTON_HEIGHT * 2), self.frame.size.width, BUTTON_HEIGHT);
         [self.createWalletButton setTitle:BC_STRING_NEW_WALLET forState:UIControlStateNormal];
         [self.createWalletButton setBackgroundColor:COLOR_BLOCKCHAIN_BLUE];
         [self addSubview:self.createWalletButton];
+        self.createWalletButton.enabled = NO;
+        self.createWalletButton.alpha = 0.0;
         
         self.existingWalletButton = [UIButton buttonWithType:UIButtonTypeCustom];
         [self.existingWalletButton setTitle:@"Existing Wallet" forState:UIControlStateNormal];
         self.existingWalletButton.frame = CGRectMake(0, self.frame.size.height - BUTTON_HEIGHT, self.frame.size.width, BUTTON_HEIGHT);
         [self.existingWalletButton setBackgroundColor:COLOR_BLOCKCHAIN_LIGHT_BLUE];
         [self addSubview:self.existingWalletButton];
+        self.existingWalletButton.enabled = NO;
+        self.existingWalletButton.alpha = 0.0;
     }
     
     return self;
+}
+
+- (void)didMoveToSuperview
+{
+    // Some nice animations
+    [UIView animateWithDuration:2*ANIMATION_DURATION
+                     animations:^{
+                         // Fade in logo
+                         imageView.alpha = 1.0;
+                     }
+                     completion:^(BOOL finished){
+                         [UIView animateWithDuration:2*ANIMATION_DURATION
+                                          animations:^{
+                                              // Move logo up
+                                              CGRect newFrame = imageView.frame;
+                                              newFrame.origin.y -= moveImageDown;
+                                              imageView.frame = newFrame;
+                                          }
+                                          completion:^(BOOL finished){
+                                              [UIView animateWithDuration:2*ANIMATION_DURATION animations:^{
+                                                  // Fade in controls
+                                                  self.createWalletButton.alpha = 1.0;
+                                                  self.existingWalletButton.alpha = 1.0;
+                                              } completion:^(BOOL finished) {
+                                                  // Activate controls
+                                                  self.createWalletButton.enabled = YES;
+                                                  self.existingWalletButton.enabled = YES;
+                                              }];
+                                          }];
+                     }];
 }
 
 @end

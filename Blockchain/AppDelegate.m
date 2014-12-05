8cf55ff5a4a6c45994997cad234fda33f8127f02
Mark Pfluger <mark@pfluegerworld.de>
2014-12-05 13:57:44 +0000

    Removed unnecessary changes from 2.1.4 merge: no more new legacy address generation for HD

--- Blockchain/AppDelegate.m
+++ Blockchain/AppDelegate.m
@@ -404,9 +404,6 @@ SideMenuViewController *sideMenuViewController;
         [_sendViewController reset];
     }
     
-    // Cancel Notification for new address on receive coins view controller (bug when second password requested and app is backgrounded)
-    [[NSNotificationCenter defaultCenter] removeObserver:_receiveViewController name:EVENT_NEW_ADDRESS object:nil];
-    
     // Show the LaunchImage so the list of running apps does not show the user's information
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.25 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         // Small delay so we don't change the view while it's zooming out

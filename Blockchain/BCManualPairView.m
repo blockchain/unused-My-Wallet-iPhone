ad839bb923cf7098b5591d8a4c0f08c65ffd042a
Mark Pfluger <mark@pfluegerworld.de>
2015-02-16 13:25:55 -0500

    New UI: Manual pairing

--- Blockchain/BCManualPairView.m
+++ Blockchain/BCManualPairView.m
@@ -16,6 +16,22 @@
     walletIdentifierTextField.delegate = self;
     passwordTextField.delegate = self;
     
+    UIButton *saveButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    saveButton.frame = CGRectMake(0, 0, self.window.frame.size.width, 46);
+    saveButton.backgroundColor = COLOR_BLOCKCHAIN_BLUE;
+    [saveButton setTitle:BC_STRING_CONTINUE forState:UIControlStateNormal];
+    [saveButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    saveButton.titleLabel.font = [UIFont systemFontOfSize:17.0];
+    
+    [saveButton addTarget:self action:@selector(continueClicked:) forControlEvents:UIControlEventTouchUpInside];
+    
+    walletIdentifierTextField.inputAccessoryView = saveButton;
+    passwordTextField.inputAccessoryView = saveButton;
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [walletIdentifierTextField becomeFirstResponder];
+    });
+    
     // Get the session id SID from the server
     [app.wallet loadWalletLogin];
 }
@@ -24,6 +40,9 @@
 {
     walletIdentifierTextField.delegate = nil;
     passwordTextField.delegate = nil;
+    
+    walletIdentifierTextField.inputAccessoryView = nil;
+    passwordTextField.inputAccessoryView = nil;
 }
 
 - (void)modalWasDismissed
@@ -45,8 +64,8 @@
 
 - (IBAction)continueClicked:(id)sender
 {
-    NSString * guid = walletIdentifierTextField.text;
-    NSString * password = passwordTextField.text;
+    NSString *guid = walletIdentifierTextField.text;
+    NSString *password = passwordTextField.text;
     
     if ([guid length] != 36) {
         [app standardNotify:BC_STRING_ENTER_YOUR_CHARACTER_WALLET_IDENTIFIER title:BC_STRING_INVALID_IDENTIFIER delegate:nil];
--- Blockchain/LocalizationConstants.h
+++ Blockchain/LocalizationConstants.h
@@ -133,6 +133,7 @@
 
 #define BC_STRING_DONE NSLocalizedString(@"Done", nil)
 #define BC_STRING_SAVE NSLocalizedString(@"Save", nil)
+#define BC_STRING_CONTINUE NSLocalizedString(@"Continue", nil)
 
 #define BC_STRING_MY_ACCOUNTS NSLocalizedString(@"My Accounts", nil)
 #define BC_STRING_TOTAL_BALANCE NSLocalizedString(@"Total Balance", nil)
